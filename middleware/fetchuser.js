fe90ad5c239b21647d2c4346f71c32061934b630
Saif Ullah <saif.rixtysoft@gmail.com>
2023-12-06 11:42:03 +0500

    Fetch user

--- middleware/fetchuser.js
+++ middleware/fetchuser.js
@@ -15,4 +15,18 @@ const fetchUser = (req, res, next) => {
   }
 };
 
+const fetchUser1 = (req, res, next) => {
+  const authToken = req.header("auth-token");
+  if (!authToken) {
+    res.status(401).send({ error: "Access Denied" });
+  }
+  try {
+    const data = jwt.verify(authToken, secKey);
+    req.user = data.user;
+    next();
+  } catch (error) {
+    return res.status(401).send({ error: "Access Denied" });
+  }
+};
+
 module.exports = fetchUser;
